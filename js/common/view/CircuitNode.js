2e838ef45feaa309fbd5f41e37df852ec90376b0
samreid <reids@colorado.edu>
2016-02-29 23:01:32 -0700

    Updated TODOs

--- js/common/view/CircuitNode.js
+++ js/common/view/CircuitNode.js
@@ -242,8 +242,6 @@ define( function( require ) {
         vertices[ i ].unsnappedPosition = vertices[ i ].unsnappedPosition.plus( unsnappedDelta );
       }
 
-      // TODO: There is a hitch when dragging two connected batteries
-
       // Is there a nearby vertex any of these could snap to?  If so, move to its location temporarily.
       // Find drop targets for *any* of the dragged vertices
       var bestDropTarget = this.getBestDropTarget( vertices );
