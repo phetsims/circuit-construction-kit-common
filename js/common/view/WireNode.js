a7a4f5fec159e2fa2d5d300ea61979721d3d720b
samreid <reids@colorado.edu>
2016-03-15 20:49:55 -0600

    fixed lint issue

--- js/common/view/WireNode.js
+++ js/common/view/WireNode.js
@@ -10,7 +10,6 @@ define( function( require ) {
 
   // modules
   var inherit = require( 'PHET_CORE/inherit' );
-  var Node = require( 'SCENERY/nodes/Node' );
   var Line = require( 'SCENERY/nodes/Line' );
   var SimpleDragHandler = require( 'SCENERY/input/SimpleDragHandler' );
   var CircuitConstructionKitBasicsConstants = require( 'CIRCUIT_CONSTRUCTION_KIT_BASICS/CircuitConstructionKitBasicsConstants' );
