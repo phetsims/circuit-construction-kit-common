250b0e472c7d6627c9d5a09018935b085afdc286
samreid <reids@colorado.edu>
2016-11-02 11:24:58 -0600

    Fixed lint issues, see https://github.com/phetsims/phet-io/issues/724

--- js/explore/model/ExploreScreenModel.js
+++ js/explore/model/ExploreScreenModel.js
@@ -13,19 +13,12 @@ define( function( require ) {
   var circuitConstructionKitCommon = require( 'CIRCUIT_CONSTRUCTION_KIT_COMMON/circuitConstructionKitCommon' );
   var CircuitConstructionKitModel = require( 'CIRCUIT_CONSTRUCTION_KIT_COMMON/common/model/CircuitConstructionKitModel' );
   var inherit = require( 'PHET_CORE/inherit' );
-  var CircuitConstructionKitQueryParameters = require( 'CIRCUIT_CONSTRUCTION_KIT_COMMON/CircuitConstructionKitQueryParameters' );
-  var Property = require( 'AXON/Property' );
-
-  // phet-io modules
-  var TBoolean = require( 'ifphetio!PHET_IO/types/TBoolean' );
 
   /**
    * @constructor
    */
   function ExploreScreenModel( tandem ) {
-
     CircuitConstructionKitModel.call( this, tandem );
-
   }
 
   circuitConstructionKitCommon.register( 'ExploreScreenModel', ExploreScreenModel );
