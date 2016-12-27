559b9c0a682e4c16ed368e462d2bfdf083203a4e
samreid <reids@colorado.edu>
2016-12-27 11:46:07 -0700

    Added docs, see https://github.com/phetsims/circuit-construction-kit-common/issues/59

--- js/common/model/Ammeter.js
+++ js/common/model/Ammeter.js
@@ -1,8 +1,8 @@
 // Copyright 2016, University of Colorado Boulder
-// TODO: Review, document, annotate, i18n, bring up to standards
 
 /**
- * Model for the Ammeter, which adds the probe position and current readout
+ * Model for the Ammeter, which adds the probe position and current readout.  There is only one ammeter per screen and
+ * it is shown/hidden.  Hence it does not need a dispose() implementation.
  *
  * @author Sam Reid (PhET Interactive Simulations)
  */
@@ -20,27 +20,34 @@ define( function( require ) {
   var TNumber = require( 'ifphetio!PHET_IO/types/TNumber' );
   var TVector2 = require( 'ifphetio!PHET_IO/types/dot/TVector2' );
 
+  /**
+   * @param {Tandem} tandem
+   * @constructor
+   */
   function Ammeter( tandem ) {
 
     Meter.call( this, tandem );
 
-    // @public Null means no reading, otherwise {number} amps
+    // @public {number|null} the full-precision reading on the ammeter
     this.currentProperty = new Property( null, {
       tandem: tandem.createTandem( 'currentProperty' ),
       phetioValueType: TNumber( { units: 'amperes' } )
     } );
+
+    // @public {Vector2} the position of the tip of the probe
     this.probePositionProperty = new Property( new Vector2(), {
       tandem: tandem.createTandem( 'probePositionProperty' ),
       phetioValueType: TVector2
     } );
-
-    Property.preventGetSet( this, 'current' );
-    Property.preventGetSet( this, 'probePosition' );
   }
 
   circuitConstructionKitCommon.register( 'Ammeter', Ammeter );
 
   return inherit( Meter, Ammeter, {
+
+    /**
+     * @public - reset the ammeter
+     */
     reset: function() {
       Meter.prototype.reset.call( this );
       this.currentProperty.reset();
