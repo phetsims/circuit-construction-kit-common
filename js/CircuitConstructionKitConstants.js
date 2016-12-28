2ee0469293d4fd6c2510999ced656f5be66976f0
samreid <reids@colorado.edu>
2016-12-28 14:32:36 -0700

    Reformatted constants

--- js/CircuitConstructionKitConstants.js
+++ js/CircuitConstructionKitConstants.js
@@ -23,18 +23,44 @@ define( function( require ) {
   circuitConstructionKitCommon.register( 'CircuitConstructionKitConstants', CircuitConstructionKitConstants );
 
   return inherit( Object, CircuitConstructionKitConstants, {}, {
-    TOOLBOX_ICON_SIZE: 60, // Maximum size for Width or height of icons in the circuit element toolbox or sensor toolbox
-    TOOLBOX_ITEM_SPACING: 30, // Spacing between adjacent items in the circuit element toolbox or sensor toolbox
-    DEFAULT_RESISTANCE: 4.5, // The resistance of a default resistor, also used in icons
-    LAYOUT_INSET: 14, // Padding for placement of control panels
-    TAP_THRESHOLD: 10, // Number of pixels (screen coordinates) that constitutes a tap instead of a drag
-    FONT_AWESOME_ICON_SCALE: 0.85, // Uniform scaling for all font awesome node button icons
-    HIGHLIGHT_COLOR: new Color( 'yellow' ), // Color for selected objects (CircuitElement and Vertex)
-    HIGHLIGHT_LINE_WIDTH: 5, // Line width for highlighting for selected objects
-    DEFAULT_RESISTIVITY: 1E-4, // Default resistivity for Wires and Switches (whose resistance varies with length)
-    MINIMUM_RESISTANCE: 1E-8, // The lowest resistance a Wire or Switch can have
-    DRAG_BOUNDS_EROSION: 10, // How far to erode the visible bounds for keeping the probes in bounds.
-    ELECTRON_SEPARATION: 28, // Distance between adjacent electrons for electron layout
-    BACKGROUND_COLOR: new Color( '#c6dbf9' ) // Color of the background
+
+    // Maximum size for Width or height of icons in the circuit element toolbox or sensor toolbox
+    TOOLBOX_ICON_SIZE: 60,
+
+    // Spacing between adjacent items in the circuit element toolbox or sensor toolbox
+    TOOLBOX_ITEM_SPACING: 30,
+
+    // The resistance of a default resistor, also used in icons
+    DEFAULT_RESISTANCE: 4.5,
+
+    // Padding for placement of control panels
+    LAYOUT_INSET: 14,
+
+    // Number of pixels (screen coordinates) that constitutes a tap instead of a drag
+    TAP_THRESHOLD: 10,
+
+    // Uniform scaling for all font awesome node button icons
+    FONT_AWESOME_ICON_SCALE: 0.85,
+
+    // Color for selected objects (CircuitElement and Vertex)
+    HIGHLIGHT_COLOR: new Color( 'yellow' ),
+
+    // Line width for highlighting for selected objects
+    HIGHLIGHT_LINE_WIDTH: 5,
+
+    // Default resistivity for Wires and Switches (whose resistance varies with length)
+    DEFAULT_RESISTIVITY: 1E-4,
+
+    // The lowest resistance a Wire or Switch can have
+    MINIMUM_RESISTANCE: 1E-8,
+
+    // How far to erode the visible bounds for keeping the probes in bounds.
+    DRAG_BOUNDS_EROSION: 10,
+
+    // Distance between adjacent electrons for electron layout
+    ELECTRON_SEPARATION: 28,
+
+    // Color of the background
+    BACKGROUND_COLOR: new Color( '#c6dbf9' )
   } );
 } );
