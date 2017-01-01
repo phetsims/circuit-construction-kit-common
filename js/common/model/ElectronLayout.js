0c4e7f2344125c047ad516052e6dc7c65905cb79
samreid <reids@colorado.edu>
2017-01-01 16:49:25 -0700

    Minor cleanup, see https://github.com/phetsims/circuit-construction-kit-common/issues/59

--- js/common/model/ElectronLayout.js
+++ js/common/model/ElectronLayout.js
@@ -1,7 +1,7 @@
 // Copyright 2016, University of Colorado Boulder
-// TODO: Review, document, annotate, i18n, bring up to standards
 
 /**
+ * Creates Electrons in a CircuitElement when it has been created, or when an adjacent wire's length has been modified.
  *
  * @author Sam Reid (PhET Interactive Simulations)
  */
@@ -14,48 +14,51 @@ define( function( require ) {
   var Electron = require( 'CIRCUIT_CONSTRUCTION_KIT_COMMON/common/model/Electron' );
   var CircuitConstructionKitConstants = require( 'CIRCUIT_CONSTRUCTION_KIT_COMMON/CircuitConstructionKitConstants' );
 
-  // constants
-  var ELECTRON_SEPARATION = CircuitConstructionKitConstants.ELECTRON_SEPARATION;
-
+  /**
+   * @param {Circuit} circuit
+   * @constructor
+   */
   function ElectronLayout( circuit ) {
+
+    // @private
     this.circuit = circuit;
-    this.electrons = circuit.electrons;
   }
 
   circuitConstructionKitCommon.register( 'ElectronLayout', ElectronLayout );
 
   return inherit( Object, ElectronLayout, {
+
+    /**
+     * Creates and positions electrons in the specified circuit element.
+     * @param circuitElement
+     * @public
+     */
     layoutElectrons: function( circuitElement ) {
 
       // Remove any electrons that were already in the branch.
-      // TODO: a performance improvement could be to adjust them instead of delete/recreate
-      // TODO: this could particularly help when dragging a wire, and the electrons are continually re-layed-out.
-      var particlesInBranch = this.circuit.getElectronsInCircuitElement( circuitElement );
-      for ( var k = 0; k < particlesInBranch.length; k++ ) {
-        particlesInBranch[ k ].dispose();
-      }
-      this.electrons.removeAll( particlesInBranch );
+      // TODO: a performance improvement could be to adjust them instead of delete/recreate. This could particularly
+      // help when dragging a wire, and the electrons are continually re-layed-out.
+      var particlesToRemove = this.circuit.getElectronsInCircuitElement( circuitElement );
+      this.circuit.electrons.removeAll( particlesToRemove );
 
       // compress or expand, but fix a particle at startingPoint and endingPoint.
-      var offset = ELECTRON_SEPARATION / 2;
+      var offset = CircuitConstructionKitConstants.ELECTRON_SEPARATION / 2;
 
-      var circuitLength = circuitElement.electronPathLength;
-      var endingPoint = circuitLength - offset;
+      var endingPoint = circuitElement.electronPathLength - offset;
       var startingPoint = offset;
       var length = endingPoint - startingPoint;
 
-      var numberParticles = length / ELECTRON_SEPARATION;
-      var integralNumberParticles = Math.ceil( numberParticles );
-      var density = ( integralNumberParticles - 1) / length;
+      var numberOfParticles = Math.ceil( length / CircuitConstructionKitConstants.ELECTRON_SEPARATION );
+      var density = ( numberOfParticles - 1) / length;  // TODO: why is this subtracting 1?
       var dx = 1 / density;
 
       // If there is a single particle, show it in the middle of the component.
-      if ( integralNumberParticles === 1 ) {
+      if ( numberOfParticles === 1 ) {
         dx = 0;
         offset = (startingPoint + endingPoint) / 2;
       }
-      for ( var i = 0; i < integralNumberParticles; i++ ) {
-        this.electrons.add( new Electron( circuitElement, i * dx + offset, this.circuit.showElectronsProperty ) );
+      for ( var i = 0; i < numberOfParticles; i++ ) {
+        this.circuit.electrons.add( new Electron( circuitElement, i * dx + offset, this.circuit.showElectronsProperty ) );
       }
 
       circuitElement.dirty = false;
