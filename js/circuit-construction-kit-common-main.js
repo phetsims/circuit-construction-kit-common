51884f411dd836f4f1f6baae89744895350f08f8
zepumph <michael.kauzmann@colorado.edu>
2017-03-13 16:48:21 -0600

    removed uses of PHET_IO/simulations/, https://github.com/phetsims/phet-io/issues/1032

--- js/circuit-construction-kit-common-main.js
+++ js/circuit-construction-kit-common-main.js
@@ -14,9 +14,6 @@ define( function( require ) {
   var SimLauncher = require( 'JOIST/SimLauncher' );
   var Tandem = require( 'TANDEM/Tandem' );
 
-  // If running as phet-io, load the API
-  require( 'ifphetio!PHET_IO/simulations/circuit-construction-kit-black-box-study/circuit-construction-kit-api' );
-
   // constants
   var TANDEM = Tandem.createRootTandem();
 
