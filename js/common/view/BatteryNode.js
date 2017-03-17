03c7587db7a9604c1106441ce3c23374998ce0f1
samreid <reids@colorado.edu>
2017-03-17 10:32:13 -0600

    Improved dimension of schematic battery, see https://github.com/phetsims/circuit-construction-kit-common/issues/236

--- js/common/view/BatteryNode.js
+++ js/common/view/BatteryNode.js
@@ -16,6 +16,7 @@ define( function( require ) {
   var Image = require( 'SCENERY/nodes/Image' );
   var Path = require( 'SCENERY/nodes/Path' );
   var Shape = require( 'KITE/Shape' );
+  var Matrix3 = require( 'DOT/Matrix3' );
 
   // images
   var batteryImage = require( 'mipmap!CIRCUIT_CONSTRUCTION_KIT_COMMON/battery.png' );
@@ -29,22 +30,31 @@ define( function( require ) {
 
     // Points sampled using Photoshop from a raster of the IEEE icon seen at https://upload.wikimedia.org/wikipedia/commons/c/cb/Circuit_elements.svg
     var batteryImageNode = new Image( batteryImage );
-    var y = 99;
+    var y = batteryImageNode.height + 6;
+    var x = -47;
     var schematicShape = new Shape()
-      .moveTo( 47, y )
-      .lineTo( 123, y )
-      .moveTo( 123, 122 )
-      .lineTo( 123, 74 )
-      .moveTo( 156, y )
-      .lineTo( 235, y )
-      .moveTo( 156, 151 )
-      .lineTo( 156, 46 );
+      .moveTo( 47 + x, y )
+      .lineTo( 123 + x, y )
+      .moveTo( 123 + x, 122 - 99 + y )
+      .lineTo( 123 + x, 74 - 99 + y )
+      .moveTo( 156 + x, y )
+      .lineTo( 235 + x, y )
+      .moveTo( 156 + x, 151 - 99 + y )
+      .lineTo( 156 + x, 46 - 99 + y );
+    var width = schematicShape.bounds.width;
+    var desiredWidth = batteryImageNode.width;
+    var scale = desiredWidth / width;
+    var scaleMatrix = Matrix3.scale( scale, scale );
+    schematicShape = schematicShape.transformed( scaleMatrix );
+    var plusShape = new Shape().moveTo( 191 + x, 64 - 99 + y ).lineTo( 191 + x, 81 - 99 + y ).moveTo( 178 + x, 73 - 99 + y ).lineTo( 203 + x, 73 - 99 + y );
+    plusShape = plusShape.transformed( scaleMatrix );
+    var path2 = new Path( plusShape, {
+      stroke: 'black', lineWidth: 4
+    } );
     var schematicNode = new Path( schematicShape, {
       stroke: 'black', lineWidth: 6,
       children: [
-        new Path( new Shape().moveTo( 191, 64 ).lineTo( 191, 81 ).moveTo( 178, 73 ).lineTo( 203, 73 ), {
-          stroke: 'black', lineWidth: 4
-        } )
+        path2
       ]
     } );
 
