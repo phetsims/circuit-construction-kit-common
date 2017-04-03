5979e7bb9c29c323d3bc8c282049e3e1e1c37c8c
samreid <reids@colorado.edu>
2017-04-02 20:54:12 -0600

    Refactoring in ElectronNode, see https://github.com/phetsims/circuit-construction-kit-common/issues/59

--- js/common/view/ChargeNode.js
+++ js/common/view/ChargeNode.js
@@ -2,8 +2,8 @@
 // TODO: Review, document, annotate, i18n, bring up to standards
 
 /**
- * Renders a single charge.  Electrons are shown as a sphere with a minus sign and conventional current is shown as an
- * arrow.  Electrons are shown when current is off, but conventional current is not.
+ * Renders a single charge. Electrons are shown as a sphere with a minus sign and conventional current is shown as an
+ * arrow.  Electrons are shown when current is zero, but conventional current is not shown for zero current.
  *
  * @author Sam Reid (PhET Interactive Simulations)
  */
@@ -22,13 +22,14 @@ define( function( require ) {
   var ArrowNode = require( 'SCENERY_PHET/ArrowNode' );
 
   // constants
-  var RADIUS = 10;
-  var SCALE = 2; // Scale up before rasterization so it won't be too pixellated/fuzzy
+  var ELECTRON_RADIUS = 10; // in view coordinates
+  var ELECTRON_SCALE = 2; // Scale up before rasterization so it won't be too pixellated/fuzzy
 
   // Copied from John Travoltage
-  var minusChargeNode = new Node( {
+  // TODO: Factor out to scenery phet?
+  var MINUS_CHARGE_NODE = new Node( {
     children: [
-      new Circle( RADIUS, {
+      new Circle( ELECTRON_RADIUS, {
         boundsMethod: 'none',
         fill: new RadialGradient( 2, -3, 2, 2, -3, 7 )
           .addColorStop( 0, '#4fcfff' )
@@ -42,22 +43,22 @@ define( function( require ) {
         centerY: 0
       } )
     ],
-    scale: SCALE,
+    scale: ELECTRON_SCALE,
     boundsMethod: 'none'
   } );
-  minusChargeNode.top = 0;
-  minusChargeNode.left = 0;
+  MINUS_CHARGE_NODE.top = 0;
+  MINUS_CHARGE_NODE.left = 0;
 
-  var electronImageNode = new Node();
-  minusChargeNode.toImage( function( im ) {
+  var ELECTRON_IMAGE_NODE = new Node();
+  MINUS_CHARGE_NODE.toImage( function( im ) {
 
     //Scale back down so the image will be the desired size
-    electronImageNode.children = [ new Image( im, { scale: 1.0 / SCALE } ) ];
-  }, 0, 0, minusChargeNode.width, minusChargeNode.height );
+    ELECTRON_IMAGE_NODE.children = [ new Image( im, { scale: 1.0 / ELECTRON_SCALE } ) ];
+  }, 0, 0, MINUS_CHARGE_NODE.width, MINUS_CHARGE_NODE.height );
 
   // Center arrow so it is easy to rotate
-  var arrowLength = 23;
-  var arrowNode = new ArrowNode( -arrowLength / 2, 0, arrowLength / 2, 0, {
+  var ARROW_LENGTH = 23; // view coordinates
+  var ARROW_NODE = new ArrowNode( -ARROW_LENGTH / 2, 0, ARROW_LENGTH / 2, 0, {
     headHeight: 10,
     headWidth: 12,
     tailWidth: 3,
@@ -69,14 +70,15 @@ define( function( require ) {
     var self = this;
     this.charge = charge;
     Node.call( this, {
-      children: [ charge.charge > 0 ? arrowNode : electronImageNode ],
+      children: [ charge.charge > 0 ? ARROW_NODE : ELECTRON_IMAGE_NODE ],
       pickable: false
     } );
     var outsideOfBlackBoxProperty = new BooleanProperty( false );
 
     // TODO: When I wrote this with Property.multilink, it failed as #172
     var updateVisible = function() {
-      self.visible = charge.visibleProperty.value && (outsideOfBlackBoxProperty.value || revealingProperty.value) &&
+      self.visible = charge.visibleProperty.value &&
+                     (outsideOfBlackBoxProperty.value || revealingProperty.value) &&
                      ( Math.abs( charge.circuitElement.currentProperty.get() ) > 1E-6 || charge.charge < 0 );
     };
     var positionListener = function( position ) {
@@ -92,10 +94,10 @@ define( function( require ) {
     charge.visibleProperty.link( updateVisible );
     outsideOfBlackBoxProperty.link( updateVisible );
 
-    var disposeListener = function() {
+    var disposeChargeNode = function() {
       self.detach();
       charge.positionProperty.unlink( positionListener );
-      charge.disposeEmitter.removeListener( disposeListener );
+      charge.disposeEmitter.removeListener( disposeChargeNode );
       revealingProperty.unlink( updateVisible );
       charge.visibleProperty.unlink( updateVisible );
       outsideOfBlackBoxProperty.unlink( updateVisible );
@@ -103,7 +105,7 @@ define( function( require ) {
       // We must remove the image child node, or it will continue to track its parents and lead to a memory leak
       self.removeAllChildren();
     };
-    charge.disposeEmitter.addListener( disposeListener );
+    charge.disposeEmitter.addListener( disposeChargeNode );
   }
 
   circuitConstructionKitCommon.register( 'ChargeNode', ChargeNode );
