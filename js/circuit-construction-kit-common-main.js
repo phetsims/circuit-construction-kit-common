f2522d36e5850c0559d3420a724c56dc113f52ad
samreid <reids@colorado.edu>
2017-04-04 12:29:37 -0600

    Updated stub to help with unit tests

--- js/circuit-construction-kit-common-main.js
+++ js/circuit-construction-kit-common-main.js
@@ -1,7 +1,7 @@
 // Copyright 2015-2016, University of Colorado Boulder
 
 /**
- * Main entry point for the sim.
+ * Stub to help in loading the unit tests.
  *
  * @author Sam Reid (PhET Interactive Simulations)
  */
@@ -9,36 +9,5 @@ define( function( require ) {
   'use strict';
 
   // modules
-  var ExploreScreen = require( 'CIRCUIT_CONSTRUCTION_KIT_COMMON/explore/ExploreScreen' );
-  var Sim = require( 'JOIST/Sim' );
-  var SimLauncher = require( 'JOIST/SimLauncher' );
-  var Tandem = require( 'TANDEM/Tandem' );
-
-  // constants
-  var TANDEM = Tandem.createRootTandem();
-
-  // strings
-  var circuitConstructionKitTitleString = require( 'string!CIRCUIT_CONSTRUCTION_KIT_COMMON/circuit-construction-kit.title' );
-
-  var simOptions = {
-    credits: {
-      leadDesign: 'Amy Rouinfar',
-      softwareDevelopment: 'Sam Reid',
-      team: 'Michael Dubson, Ariel Paul, Kathy Perkins',
-      qualityAssurance: 'Steele Dalton, Bryce Griebenow, Elise Morgan, Ben Roberts',
-      graphicArts: 'Bryce Gruneich'
-    }
-  };
-
-  // Circuit Construction Kit has unit tests for checking the mathematics for the Modified Nodal Analysis
-  // algorithm.  In order to load the classes into an accessible namespace, the *-config.js and *-main.js are loaded
-  // however, when running the unit tests we don't also want to launch the simulation.
-  if ( !window.circuitConstructionKitTestSuite ) {
-    SimLauncher.launch( function() {
-      var sim = new Sim( circuitConstructionKitTitleString, [
-        new ExploreScreen( TANDEM.createTandem( 'exploreScreen' ) ),
-      ], simOptions );
-      sim.start();
-    } );
-  }
+  require( 'CIRCUIT_CONSTRUCTION_KIT_COMMON/common/model/Circuit' );
 } );
