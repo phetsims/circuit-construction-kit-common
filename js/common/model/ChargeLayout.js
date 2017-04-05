75121135a7a51e47e053be527d60e393e5c9aed1
samreid <reids@colorado.edu>
2017-04-05 12:28:29 -0600

    simplified layout dirty logic

--- js/common/model/ChargeLayout.js
+++ js/common/model/ChargeLayout.js
@@ -35,37 +35,37 @@ define( function( require ) {
      */
     layoutCharges: function( circuitElement ) {
 
-      if ( !circuitElement.chargeLayoutDirty ) {
-        return;
+      // Avoid unnecessary work to improve performance
+      if ( circuitElement.chargeLayoutDirty ) {
+
+        // Remove any charges that were already in the branch.
+        // TODO: a performance improvement could be to adjust them instead of delete/recreate. This could particularly
+        // help when dragging a wire, and the charges are continually re-layed-out.
+        var particlesToRemove = this.circuit.getChargesInCircuitElement( circuitElement );
+        this.circuit.charges.removeAll( particlesToRemove );
+
+        // compress or expand, but fix a particle at startingPoint and endingPoint.
+        var offset = CircuitConstructionKitConstants.CHARGE_SEPARATION / 2;
+
+        var endingPoint = circuitElement.chargePathLength - offset;
+        var startingPoint = offset;
+        var length = endingPoint - startingPoint;
+
+        var numberOfParticles = Math.ceil( length / CircuitConstructionKitConstants.CHARGE_SEPARATION );
+        var density = ( numberOfParticles - 1) / length;  // TODO: why is this subtracting 1?
+        var dx = 1 / density;
+
+        // If there is a single particle, show it in the middle of the component.
+        if ( numberOfParticles === 1 ) {
+          dx = 0;
+          offset = (startingPoint + endingPoint) / 2;
+        }
+        for ( var i = 0; i < numberOfParticles; i++ ) {
+          this.circuit.charges.add( new Charge( circuitElement, i * dx + offset, this.circuit.showCurrentProperty, this.circuit.currentTypeProperty.get() === 'electrons' ? -1 : +1 ) );
+        }
+
+        circuitElement.chargeLayoutDirty = false;
       }
-
-      // Remove any charges that were already in the branch.
-      // TODO: a performance improvement could be to adjust them instead of delete/recreate. This could particularly
-      // help when dragging a wire, and the charges are continually re-layed-out.
-      var particlesToRemove = this.circuit.getChargesInCircuitElement( circuitElement );
-      this.circuit.charges.removeAll( particlesToRemove );
-
-      // compress or expand, but fix a particle at startingPoint and endingPoint.
-      var offset = CircuitConstructionKitConstants.CHARGE_SEPARATION / 2;
-
-      var endingPoint = circuitElement.chargePathLength - offset;
-      var startingPoint = offset;
-      var length = endingPoint - startingPoint;
-
-      var numberOfParticles = Math.ceil( length / CircuitConstructionKitConstants.CHARGE_SEPARATION );
-      var density = ( numberOfParticles - 1) / length;  // TODO: why is this subtracting 1?
-      var dx = 1 / density;
-
-      // If there is a single particle, show it in the middle of the component.
-      if ( numberOfParticles === 1 ) {
-        dx = 0;
-        offset = (startingPoint + endingPoint) / 2;
-      }
-      for ( var i = 0; i < numberOfParticles; i++ ) {
-        this.circuit.charges.add( new Charge( circuitElement, i * dx + offset, this.circuit.showCurrentProperty, this.circuit.currentTypeProperty.get() === 'electrons' ? -1 : +1 ) );
-      }
-
-      circuitElement.chargeLayoutDirty = false;
     }
   } );
 } );
