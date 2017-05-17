f114cd2521b677e7393825f624486733a88736fb
samreid <reids@colorado.edu>
2017-05-17 16:00:14 -0600

    factored out switch node creation for schematic, see https://github.com/phetsims/circuit-construction-kit-common/issues/88

--- js/common/view/SwitchNode.js
+++ js/common/view/SwitchNode.js
@@ -13,9 +13,6 @@ define( function( require ) {
   var circuitConstructionKitCommon = require( 'CIRCUIT_CONSTRUCTION_KIT_COMMON/circuitConstructionKitCommon' );
   var CircuitConstructionKitConstants = require( 'CIRCUIT_CONSTRUCTION_KIT_COMMON/CircuitConstructionKitConstants' );
   var FixedLengthCircuitElementNode = require( 'CIRCUIT_CONSTRUCTION_KIT_COMMON/common/view/FixedLengthCircuitElementNode' );
-  var Path = require( 'SCENERY/nodes/Path' );
-  var Shape = require( 'KITE/Shape' );
-  var Matrix3 = require( 'DOT/Matrix3' );
   var Rectangle = require( 'SCENERY/nodes/Rectangle' );
   var Circle = require( 'SCENERY/nodes/Circle' );
   var Node = require( 'SCENERY/nodes/Node' );
@@ -23,12 +20,6 @@ define( function( require ) {
 
   // constants
   // dimensions for schematic battery
-  var SMALL_TERMINAL_WIDTH = 50;
-  var LARGE_TERMINAL_WIDTH = 104;
-  var WIDTH = 188;
-  var GAP = 33;
-  var LEFT_JUNCTION = WIDTH / 2 - GAP / 2;
-  var RIGHT_JUNCTION = WIDTH / 2 + GAP / 2;
   var LIFELIKE_DIAMETER = 16;
 
   /**
@@ -46,69 +37,64 @@ define( function( require ) {
     // @public (read-only) - the Switch rendered by this Node
     this.circuitSwitch = circuitSwitch;
 
-    var leftSegment = new Rectangle( 0, -LIFELIKE_DIAMETER / 2, CircuitConstructionKitConstants.SWITCH_LENGTH / 3, LIFELIKE_DIAMETER, {
-      fill: '#d48270',
-      stroke: 'black',
-      lineWidth: 1
-    } );
-    var midSegment = new Rectangle( 0, -LIFELIKE_DIAMETER / 2, CircuitConstructionKitConstants.SWITCH_LENGTH / 3, LIFELIKE_DIAMETER, {
-      x: CircuitConstructionKitConstants.SWITCH_LENGTH / 3,
-      fill: '#d48270',
-      stroke: 'black',
-      lineWidth: 1
-    } );
+    /**
+     * @param {string} type - 'lifelike'|'schematic'
+     * @param {Color|string} fill
+     * @param {number} thickness
+     * @returns {Node}
+     */
+    var createNode = function( type, fill, thickness ) {
+      var leftSegment = new Rectangle( 0, -thickness / 2, CircuitConstructionKitConstants.SWITCH_LENGTH / 3, thickness, {
+        fill: fill,
+        stroke: 'black',
+        lineWidth: 1
+      } );
+      var midSegment = new Rectangle( 0, -thickness / 2, CircuitConstructionKitConstants.SWITCH_LENGTH / 3, thickness, {
+        x: CircuitConstructionKitConstants.SWITCH_LENGTH / 3,
+        fill: fill,
+        stroke: 'black',
+        lineWidth: 1
+      } );
 
-    circuitSwitch.closedProperty.link( function( closed ) {
-      midSegment.setRotation( closed ? 0 : -Math.PI / 4 );
-    } );
-    var rightSegment = new Rectangle( CircuitConstructionKitConstants.SWITCH_LENGTH * 2 / 3, -LIFELIKE_DIAMETER / 2, CircuitConstructionKitConstants.SWITCH_LENGTH / 3, LIFELIKE_DIAMETER, {
-      fill: '#d48270',
-      stroke: 'black',
-      lineWidth: 1
-    } );
+      circuitSwitch.closedProperty.link( function( closed ) {
+        midSegment.setRotation( closed ? 0 : -Math.PI / 4 );
+      } );
+      var rightSegment = new Rectangle( CircuitConstructionKitConstants.SWITCH_LENGTH * 2 / 3, -thickness / 2, CircuitConstructionKitConstants.SWITCH_LENGTH / 3, thickness, {
+        fill: fill,
+        stroke: 'black',
+        lineWidth: 1
+      } );
 
-    var lifelikeHinge = new Circle( LIFELIKE_DIAMETER / 2, {
-      fill: '#a7a8ab',
-      stroke: 'black',
-      lineWidth: 4,
-      x: CircuitConstructionKitConstants.SWITCH_LENGTH / 3
-    } );
-    var lifelikeNode = new Node( {
-      children: [ leftSegment, midSegment, rightSegment, lifelikeHinge ]
-    } );
+      var lifelikeHinge = new Circle( thickness * 0.6, {
+        fill: '#a7a8ab',
+        stroke: 'black',
+        lineWidth: 4,
+        x: CircuitConstructionKitConstants.SWITCH_LENGTH / 3
+      } );
 
-    // Points sampled using Photoshop from a raster of the IEEE icon seen at
-    // https://upload.wikimedia.org/wikipedia/commons/c/cb/Circuit_elements.svg
-    var schematicShape = new Shape()
-      .moveTo( 0, 0 ) // left wire
-      .lineTo( LEFT_JUNCTION, 0 )
-      .moveTo( LEFT_JUNCTION, SMALL_TERMINAL_WIDTH / 2 ) // left plate
-      .lineTo( LEFT_JUNCTION, -SMALL_TERMINAL_WIDTH / 2 )
-      .moveTo( RIGHT_JUNCTION, 0 ) // right wire
-      .lineTo( WIDTH, 0 )
-      .moveTo( RIGHT_JUNCTION, LARGE_TERMINAL_WIDTH / 2 ) // right plate
-      .lineTo( RIGHT_JUNCTION, -LARGE_TERMINAL_WIDTH / 2 );
-    var schematicWidth = schematicShape.bounds.width;
-    var desiredWidth = lifelikeNode.width;
-    var schematicScale = desiredWidth / schematicWidth;
+      var node = new Node( {
+        children: [ leftSegment, midSegment, rightSegment, lifelikeHinge ]
+      } );
 
-    // Align vertically
-    schematicShape = schematicShape.transformed( Matrix3.translation( 0, lifelikeNode.height / 2 + 7 ) );
+      if ( type === 'schematic' ) {
+        node.addChild( new Circle( thickness * 0.6, {
+          fill: 'black',
+          stroke: 'black',
+          lineWidth: 4,
+          x: CircuitConstructionKitConstants.SWITCH_LENGTH * 2 / 3
+        } ) );
+      }
 
-    // Scale to fit the correct width
-    schematicShape = schematicShape.transformed( Matrix3.scale( schematicScale, schematicScale ) );
-    var schematicNode = new Path( schematicShape, {
-      stroke: 'black',
-      lineWidth: 4
-    } );
+      // Expand the pointer areas with a defensive copy, see https://github.com/phetsims/circuit-construction-kit-common/issues/310
+      // And make it so clicking in the gap still toggles the switch
+      node.mouseArea = node.bounds.copy();
+      node.touchArea = node.bounds.copy();
 
-    // Expand the pointer areas with a defensive copy, see https://github.com/phetsims/circuit-construction-kit-common/issues/310
-    schematicNode.mouseArea = schematicNode.bounds.copy();
-    schematicNode.touchArea = schematicNode.bounds.copy();
+      return node;
+    };
 
-    // Make it so clicking in the gap still toggles the switch
-    lifelikeNode.mouseArea = lifelikeNode.bounds.copy();
-    lifelikeNode.touchArea = lifelikeNode.bounds.copy();
+    var lifelikeNode = createNode( 'lifelike', '#d48270', LIFELIKE_DIAMETER );
+    var schematicNode = createNode( 'schematic', 'black', 6 );// TODO: factor out thickness with WireNode
 
     FixedLengthCircuitElementNode.call( this,
       circuitConstructionKitScreenView,
