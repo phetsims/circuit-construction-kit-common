5135673e38e60a8c050de805f233df754e981e3d
Denz1994 <denz1994@gmail.com>
2017-06-01 12:56:22 -0600

    Added 10 more unit test to 'test resistor colors' tests. Reference#340.

--- tests/qunit/js/circuit-construction-kit-common-tests.js
+++ tests/qunit/js/circuit-construction-kit-common-tests.js
@@ -217,8 +217,18 @@
 
   QUnit.test( 'test resistor colors', function( assert ) {
     assert.deepEqual( ResistorColors.getColorNames( 0 ), [ 'black' ], '0 resistance should have one black band' );
-    assert.deepEqual( ResistorColors.getColorNames( 4700 ), [ 'yellow', 'violet', 'red', 'gold' ], '4700 ohm resistor' );
+    assert.deepEqual( ResistorColors.getColorNames( 1 ), [ 'brown', 'black', 'gold', 'gold' ], '1 ohm resistor' );
     assert.deepEqual( ResistorColors.getColorNames( 10 ), [ 'brown', 'black', 'black', 'gold' ], '10 ohm resistor' );
+    assert.deepEqual( ResistorColors.getColorNames( 100 ), [ 'brown', 'black', 'brown', 'gold' ], '100 ohm resistor' );
+    assert.deepEqual( ResistorColors.getColorNames( 1000 ), [ 'brown', 'black', 'red', 'gold' ], '100 ohm resistor' );
+    assert.deepEqual( ResistorColors.getColorNames( 4700 ), [ 'yellow', 'violet', 'red', 'gold' ], '4700 ohm resistor' );
+    assert.deepEqual( ResistorColors.getColorNames( 9900 ), [ 'white', 'white', 'red', 'gold' ], '9900 ohm resistor' );
+    assert.deepEqual( ResistorColors.getColorNames( 55 ), [ 'green', 'green', 'black', 'gold' ], '55 ohm resistor' );
+    assert.deepEqual( ResistorColors.getColorNames( 34.5 ), [ 'orange', 'green', 'black', 'gold' ], '34.5 ohm resistor' );
     assert.deepEqual( ResistorColors.getColorNames( 99.5 ), [ 'brown', 'black', 'brown', 'gold' ], '99.5 ohm resistor' );
+    assert.deepEqual( ResistorColors.getColorNames( 10.5 ), [ 'brown', 'brown', 'black', 'gold' ], '10.5 ohm resistor' );
+    assert.deepEqual( ResistorColors.getColorNames( 7.5 ), [ 'violet', 'green', 'gold', 'gold' ], '7.5 ohm resistor' );
+    assert.deepEqual( ResistorColors.getColorNames( 20 ), [ 'red', 'black', 'black', 'gold' ], '20 ohm resistor' );
+    assert.deepEqual( ResistorColors.getColorNames( 88000 ), [ 'gray', 'gray', 'orange', 'gold' ], '20 ohm resistor' );
   } );
 })();
