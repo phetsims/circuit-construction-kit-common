5ba46b33eb58e5ca0799677936fbe01ce4207403
samreid <reids@colorado.edu>
2017-08-31 10:58:04 -0600

    Added review discussion, see https://github.com/phetsims/circuit-construction-kit-common/issues/317

--- js/model/RunningAverage.js
+++ js/model/RunningAverage.js
@@ -45,9 +45,10 @@ define( function( require ) {
      * @public
      */
     updateRunningAverage: function( sample ) {
-      //REVIEW*: Probably not too important, but keeping a "total" of all samples in the array, adding/subtracting from
-      //REVIEW*: it on changes, and then dividing it by the length is more efficient (essentially 2 adds, 1 divide per
-      //REVIEW*: operation, instead of N-1 adds, 1 divide).
+      //REVIEW: Probably not too important, but keeping a "total" of all samples in the array, adding/subtracting from
+      //REVIEW: it on changes, and then dividing it by the length is more efficient (essentially 2 adds, 1 divide per
+      //REVIEW: operation, instead of N-1 adds, 1 divide).
+      //REVIEW^(samreid): How to keep track of the number to be subtracted from the total?
       this.samples.push( sample );
       while ( this.samples.length > this.windowSize ) {
         this.samples.shift();
