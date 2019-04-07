556e4364bafa840182e0217c9d6d4c8af2fe8972
samreid <reids@colorado.edu>
2019-04-06 21:20:33 -0600

    Update jsdoc

--- js/view/CCKCChartNode.js
+++ js/view/CCKCChartNode.js
@@ -48,7 +48,6 @@ define( require => {
     /**
      * @param {CircuitLayerNode} circuitLayerNode
      * @param {NumberProperty} timeProperty
-     * @param {BooleanProperty} isInPlayAreaProperty
      * @param {Property.<Bounds2>} visibleBoundsProperty
      * @param {DynamicSeries[]} seriesArray
      * @param {string} verticalAxisLabel
