497e6667a5211e0e099e504f97b1c240cd7a0a88
samreid <reids@colorado.edu>
2019-11-18 14:36:58 -0700

    Make ResetFuseButton extend CCKCRoundPushButton, see https://github.com/phetsims/circuit-construction-kit-common/issues/531

--- js/view/ResetFuseButton.js
+++ js/view/ResetFuseButton.js
@@ -9,16 +9,15 @@ define( require => {
   'use strict';
 
   // modules
+  const CCKCRoundPushButton = require( 'CIRCUIT_CONSTRUCTION_KIT_COMMON/view/CCKCRoundPushButton' );
   const Circle = require( 'SCENERY/nodes/Circle' );
   const circuitConstructionKitCommon = require( 'CIRCUIT_CONSTRUCTION_KIT_COMMON/circuitConstructionKitCommon' );
   const Node = require( 'SCENERY/nodes/Node' );
   const Path = require( 'SCENERY/nodes/Path' );
-  const PhetColorScheme = require( 'SCENERY_PHET/PhetColorScheme' );
-  const RoundPushButton = require( 'SUN/buttons/RoundPushButton' );
   const Shape = require( 'KITE/Shape' );
   const Vector2 = require( 'DOT/Vector2' );
 
-  class ResetFuseButton extends RoundPushButton {
+  class ResetFuseButton extends CCKCRoundPushButton {
 
     /**
      * @param {Fuse} fuse - the Fuse to reset
@@ -28,29 +27,17 @@ define( require => {
 
       const shape = new Shape().moveTo( 0, 0 ).zigZagToPoint( new Vector2( 35, 0 ), 4.7, 4, false );
 
-      super( {
-        baseColor: PhetColorScheme.BUTTON_YELLOW,
-        content: new Node( {
-          children: [ new Path( shape, {
-            stroke: 'black',
-            lineWidth: 1.2,
-            centerX: 0,
-            centerY: 0
-          } ), new Circle( 2.2, { fill: 'black', centerX: 0, centerY: 0 } ) ]
-        } ),
-        listener: () => fuse.resetFuse(),
-
-        // Adjusted until the ResetFuseButton is the same height as the trash can button
-        minXMargin: 8,
-        minYMargin: 8,
-        tandem: tandem,
-
-        // TODO: are these unnecessary?  Could we just uninstrument as we did for NumberControl?
-        phetioState: false,
-        phetioComponentOptions: {
-          phetioState: false
-        }
+      const icon = new Node( {
+        children: [ new Path( shape, {
+          stroke: 'black',
+          lineWidth: 1.2,
+          centerX: 0,
+          centerY: 0
+        } ), new Circle( 2.2, { fill: 'black', centerX: 0, centerY: 0 } ) ],
+        scale: 0.9 // to match the size of the trash can icon
       } );
+
+      super( icon, () => fuse.resetFuse(), tandem );
       fuse.isTrippedProperty.link( isTripped => {
         this.setEnabled( isTripped );
       } );
