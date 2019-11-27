bb9214320fb03feece7bd2c944d2b6d33fa31ef3
samreid <reids@colorado.edu>
2019-11-26 21:35:49 -0700

    Add wire segments for the lifelike inductor node, see https://github.com/phetsims/circuit-construction-kit-common/issues/537

--- js/view/InductorNode.js
+++ js/view/InductorNode.js
@@ -31,17 +31,27 @@ define( require => {
   const schematicShape = new Shape()
     .moveTo( 0, 0 ) // left wire
     .lineTo( SCHEMATIC_MARGIN, 0 )
-    .arc( SCHEMATIC_MARGIN + SCHEMATIC_ARC_RADIUS * 1, 0, SCHEMATIC_ARC_RADIUS, Math.PI, 0, false ) // left plate
-    .arc( SCHEMATIC_MARGIN + SCHEMATIC_ARC_RADIUS * 3, 0, SCHEMATIC_ARC_RADIUS, Math.PI, 0, false ) // left plate
-    .arc( SCHEMATIC_MARGIN + SCHEMATIC_ARC_RADIUS * 5, 0, SCHEMATIC_ARC_RADIUS, Math.PI, 0, false ) // left plate
-    .arc( SCHEMATIC_MARGIN + SCHEMATIC_ARC_RADIUS * 7, 0, SCHEMATIC_ARC_RADIUS, Math.PI, 0, false ) // left plate
+    .arc( SCHEMATIC_MARGIN + SCHEMATIC_ARC_RADIUS * 1, 0, SCHEMATIC_ARC_RADIUS, Math.PI, 0, false )
+    .arc( SCHEMATIC_MARGIN + SCHEMATIC_ARC_RADIUS * 3, 0, SCHEMATIC_ARC_RADIUS, Math.PI, 0, false )
+    .arc( SCHEMATIC_MARGIN + SCHEMATIC_ARC_RADIUS * 5, 0, SCHEMATIC_ARC_RADIUS, Math.PI, 0, false )
+    .arc( SCHEMATIC_MARGIN + SCHEMATIC_ARC_RADIUS * 7, 0, SCHEMATIC_ARC_RADIUS, Math.PI, 0, false )
     .lineTo( SCHEMATIC_WIDTH, 0 );
 
   const LIFELIKE_HEIGHT = 60;
   const LIFELIKE_WIDTH = CCKCConstants.INDUCTOR_LENGTH;
   const LIFELIKE_RADIUS_X = 5;
   const LIFELIKE_RADIUS_Y = LIFELIKE_HEIGHT / 2;
-  const LIFELIKE_WIRE_LINE_WIDTH = 3;
+  const LIFELIKE_WIRE_LINE_WIDTH = 4;
+  const LIFELIKE_PATH_OUTLINE_STYLES = new LineStyles( {
+    lineWidth: LIFELIKE_WIRE_LINE_WIDTH,
+    lineCap: 'round',
+    lineJoin: 'round'
+  } );
+  const LIFELIKE_PATH_FILL_STYLES = new LineStyles( {
+    lineWidth: LIFELIKE_WIRE_LINE_WIDTH - 1.5,
+    lineCap: 'round',
+    lineJoin: 'round'
+  } );
 
   class InductorNode extends FixedCircuitElementNode {
 
@@ -77,22 +87,37 @@ define( require => {
       const wireWrapNode = new Node();
       inductor.inductanceProperty.link( inductance => {
 
+        // Determine the number of loops, including the start and end segments, which are each half.
         const numLoops = Util.roundSymmetric( Util.linear( 10, 100, 5, 20, inductance ) );
         const children = [];
         for ( let i = 0; i < numLoops; i++ ) {
-          const wireShape = new Shape()
-            .ellipticalArc( 0, LIFELIKE_HEIGHT / 2, LIFELIKE_RADIUS_X, LIFELIKE_RADIUS_Y, 0, -Math.PI / 2, Math.PI / 2, false )
-            .getStrokedShape( new LineStyles( { lineWidth: LIFELIKE_WIRE_LINE_WIDTH } ) );
-          const wirePath = new Path( wireShape, {
-            fill: '#dc9180',
-            stroke: 'black',
-            x: Util.linear(
-              numLoops / 2, numLoops / 2 + 1,
-              LIFELIKE_WIDTH / 2 + LIFELIKE_RADIUS_X / 2, LIFELIKE_WIDTH / 2 + LIFELIKE_WIRE_LINE_WIDTH + LIFELIKE_RADIUS_X / 2,
-              i )
-          } );
-          wireWrapNode.addChild( wirePath );
-          children.push( wirePath );
+
+          // Loops for the main body, with special cases for the start end end loop, which are halved.
+          const startAngle = i === numLoops - 1 ? Math.PI / 2 : -Math.PI / 2;
+          const endAngle = i === 0 || i === numLoops - 1 ? 0 : Math.PI / 2;
+          const anticounterclockwise = i === numLoops - 1;
+
+          // Positioning for the loop arc
+          const x = Util.linear(
+            numLoops / 2, numLoops / 2 + 1,
+            LIFELIKE_WIDTH / 2 + LIFELIKE_RADIUS_X / 2, LIFELIKE_WIDTH / 2 + LIFELIKE_WIRE_LINE_WIDTH + LIFELIKE_RADIUS_X / 2,
+            i );
+          const pathShape = new Shape()
+            .ellipticalArc( x, LIFELIKE_HEIGHT / 2, LIFELIKE_RADIUS_X, LIFELIKE_RADIUS_Y, 0, startAngle, endAngle, anticounterclockwise );
+
+          // Wire segments for the start and end
+          if ( i === 0 ) {
+            pathShape.lineTo( 0, LIFELIKE_HEIGHT / 2 );
+          }
+          if ( i === numLoops - 1 ) {
+            pathShape.lineTo( LIFELIKE_WIDTH + LIFELIKE_RADIUS_X, LIFELIKE_HEIGHT / 2 );
+          }
+
+          // Using a single path with fill+stroke leaves an artifact at the corners.  As a workaround, use two fills,
+          // see https://github.com/phetsims/circuit-construction-kit-common/issues/537#issuecomment-558917786
+          const createPath = ( lineStyles, fill ) => new Path( pathShape.getStrokedShape( lineStyles ), { fill: fill } );
+          children.push( createPath( LIFELIKE_PATH_OUTLINE_STYLES, 'black' ) );
+          children.push( createPath( LIFELIKE_PATH_FILL_STYLES, '#dc9180' ) );
         }
         wireWrapNode.children = children;
       } );
@@ -102,12 +127,7 @@ define( require => {
         centerY: 0
       } );
 
-      const width = options.isIcon ? CCKCConstants.INDUCTOR_LENGTH : inductor.distanceBetweenVertices;
-      lifelikeNode.mutate( {
-        scale: width / lifelikeNode.width
-      } );
-
-      const scale = lifelikeNode.width / schematicShape.bounds.width;
+      const scale = LIFELIKE_WIDTH / schematicShape.bounds.width;
 
       // Scale to fit the correct width
       const scaledShape = schematicShape.transformed( Matrix3.scale( scale, scale ) );
