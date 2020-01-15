c8a9f241ab1fd62fbc8b4c7ddc66f4689a56221b
phet-dev <phetadmin@gmail.com>
2020-01-15 03:13:38 -0700

    update copyright dates from daily grunt work

--- js/view/CircuitElementNode.js
+++ js/view/CircuitElementNode.js
@@ -1,4 +1,4 @@
-// Copyright 2016-2019, University of Colorado Boulder
+// Copyright 2016-2020, University of Colorado Boulder
 
 /**
  * Abstract base class for WireNode and FixedCircuitElementNode
