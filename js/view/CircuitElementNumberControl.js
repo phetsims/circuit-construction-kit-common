9b824781d202e6ad698d5493517b7b3c12cf701f
phet-dev <phetadmin@gmail.com>
2020-02-04 03:17:07 -0700

    update copyright dates from daily grunt work

--- js/view/CircuitElementNumberControl.js
+++ js/view/CircuitElementNumberControl.js
@@ -1,4 +1,4 @@
-// Copyright 2016-2019, University of Colorado Boulder
+// Copyright 2016-2020, University of Colorado Boulder
 
 /**
  * Shows controls for a single CircuitElement at the bottom of the screen and contained in a CircuitElementEditContainerNode.
