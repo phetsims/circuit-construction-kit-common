e6da5d8d200bd6239199cf3d942d0fa330b934c4
zepumph <michael.kauzmann@colorado.edu>
2020-04-06 16:57:05 -0800

    fix typo, https://github.com/phetsims/scenery/issues/997

--- js/view/VertexNode.js
+++ js/view/VertexNode.js
@@ -50,7 +50,7 @@ class VertexNode extends Node {
       cursor: 'pointer',
 
       // keyboard navigation
-      tagName: 'div', // HTML tag name for representative element in the document, see ParallelPDOM.js
+      tagName: 'div', // HTML tag name for representative element in the document, see ParallelDOM.js
       focusable: true,
       focusHighlight: 'invisible' // highlights are drawn by the simulation, invisible is deprecated don't use in future
     } );
