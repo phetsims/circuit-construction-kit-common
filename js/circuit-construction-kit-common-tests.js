8d3a2a0c12cf206560af05212fbba49df9330158
zepumph <michael.kauzmann@colorado.edu>
2020-05-21 13:16:33 -0800

    convert all tests to use qunitStart.js, https://github.com/phetsims/utterance-queue/issues/12#event-3334154840

--- js/circuit-construction-kit-common-tests.js
+++ js/circuit-construction-kit-common-tests.js
@@ -9,6 +9,7 @@
 import './model/DynamicCircuitTests.js';
 import './model/ModifiedNodalAnalysisCircuitTests.js';
 import './view/ResistorColorsTests.js';
+import qunitStart from '../../chipper/js/sim-tests/qunitStart.js';
 
 // Since our tests are loaded asynchronously, we must direct QUnit to begin the tests
-QUnit.start();
+qunitStart();
