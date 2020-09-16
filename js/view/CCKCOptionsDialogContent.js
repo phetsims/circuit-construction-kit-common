aa4e58d0ea48a9df87e58f3f6afadf0918e19f5c
samreid <reids@colorado.edu>
2020-09-15 22:13:19 -0600

    Improve options dialog, see https://github.com/phetsims/circuit-construction-kit-common/issues/429

--- js/view/CCKCOptionsDialogContent.js
+++ js/view/CCKCOptionsDialogContent.js
@@ -1,24 +1,37 @@
 // Copyright 2020, University of Colorado Boulder
 
+/**
+ * Shows the contents for the options dialog.
+ *
+ * @author Sam Reid (PhET Interactive Simulations)
+ */
 import Text from '../../../scenery/js/nodes/Text.js';
-import Node from '../../../scenery/js/nodes/Node.js';
+import VBox from '../../../scenery/js/nodes/VBox.js';
 import VerticalAquaRadioButtonGroup from '../../../sun/js/VerticalAquaRadioButtonGroup.js';
 import circuitConstructionKitCommon from '../circuitConstructionKitCommon.js';
 import SchematicType from './SchematicType.js';
 import schematicTypeProperty from './schematicTypeProperty.js';
 
-/**
- *
- * @author Sam Reid (PhET Interactive Simulations)
- */
-
-class CCKCOptionsDialogContent extends Node {
+class CCKCOptionsDialogContent extends VBox {
   constructor() {
-    super();
-    this.addChild( new VerticalAquaRadioButtonGroup( schematicTypeProperty, [
-      { node: new Text( 'IEEE' ), value: SchematicType.IEEE },
-      { node: new Text( 'IEC' ), value: SchematicType.IEC }
-    ] ) );
+
+    const textOptions = {
+      fontSize: 23
+    };
+    super( {
+      align: 'left',
+      spacing: 22,
+      children: [
+        new Text( 'Electronic Symbol Standard', textOptions ),
+        new VerticalAquaRadioButtonGroup( schematicTypeProperty, [
+          { node: new Text( 'IEEE', textOptions ), value: SchematicType.IEEE },
+          { node: new Text( 'IEC', textOptions ), value: SchematicType.IEC }
+        ], {
+          radioButtonOptions: {
+            radius: 9
+          }
+        } ) ]
+    } );
   }
 }
 
