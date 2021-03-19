ec55c81875211c3313e86f111ec2a57d6eb4d771
samreid <reids@colorado.edu>
2021-03-19 13:48:17 -0600

    Reduce minimum wire resistance to 1E-14, see https://github.com/phetsims/circuit-construction-kit-common/issues/676

--- js/model/Wire.js
+++ js/model/Wire.js
@@ -44,6 +44,10 @@ class Wire extends CircuitElement {
     // @public {NumberProperty} - the resistance of the Wire in ohms
     this.resistanceProperty = new NumberProperty( CCKCConstants.MINIMUM_RESISTANCE );
 
+    if ( phet.chipper.queryParameters.dev ) {
+      this.resistanceProperty.link( console.log );
+    }
+
     // @public {Property.<number>} - the resistivity of the Wire in ohm-meters
     this.resistivityProperty = resistivityProperty;
 
@@ -91,7 +95,7 @@ class Wire extends CircuitElement {
       // R = rho * L / A.  Resistance = resistivity * Length / cross sectional area.
       const resistance = this.resistivityProperty.get() * modelLength / CCKCConstants.WIRE_CROSS_SECTIONAL_AREA;
 
-      const clampedResistance = Math.max( CCKCConstants.MINIMUM_RESISTANCE, resistance );
+      const clampedResistance = Math.max( CCKCConstants.MINIMUM_WIRE_RESISTANCE, resistance );
       assert && assert( !isNaN( clampedResistance ), 'wire resistance should not be NaN' );
       this.resistanceProperty.set( clampedResistance );
 
