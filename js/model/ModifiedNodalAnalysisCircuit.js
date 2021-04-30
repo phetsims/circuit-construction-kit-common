d9ba30e9e66098638bc9af39c44beee2fed5be8d
samreid <reids@colorado.edu>
2021-04-30 10:48:00 -0600

    Improve error output, see https://github.com/phetsims/circuit-construction-kit-common/issues/694

--- js/model/ModifiedNodalAnalysisCircuit.js
+++ js/model/ModifiedNodalAnalysisCircuit.js
@@ -349,7 +349,7 @@ class ModifiedNodalAnalysisCircuit {
     for ( let i = 0; i < unknownVoltages.length; i++ ) {
       const unknownVoltage = unknownVoltages[ i ];
       const rhs = x.get( getIndexByEquals( unknowns, unknownVoltage ), 0 );
-      assert && assert( !isNaN( rhs ), 'the right-hand-side-value must be a number' );
+      assert && assert( !isNaN( rhs ), `the right-hand-side-value must be a number. Instead it was ${rhs}` );
       voltageMap[ unknownVoltage.node ] = rhs;
     }
     for ( let i = 0; i < unknownCurrents.length; i++ ) {
