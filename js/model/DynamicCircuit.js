c74bc7d6b698064931afd85dff7fff4e675926f7
samreid <reids@colorado.edu>
2021-09-14 11:30:57 -0600

    Move CircuitResult to a separate file, see https://github.com/phetsims/circuit-construction-kit-common/issues/738

--- js/model/DynamicCircuit.js
+++ js/model/DynamicCircuit.js
@@ -9,6 +9,7 @@
  */
 
 import CCKCQueryParameters from '../CCKCQueryParameters.js';
+import CircuitResult from './CircuitResult.js';
 import CCKCUtils from '../CCKCUtils.js';
 import circuitConstructionKitCommon from '../circuitConstructionKitCommon.js';
 import ModifiedNodalAnalysisCircuit from './ModifiedNodalAnalysisCircuit.js';
@@ -243,65 +244,6 @@ class DynamicCircuit {
   }
 }
 
-/**
- * This class represents the solution obtained by a timestep-subdivision-oriented MNA solve with companion models.
- * The distinction between instantaneous and average currents/voltages is made because we need to maintain the correct
- * dynamics (using instantaneous solutions) but also to show intermediate states (using the average results), see #2270.
- */
-class CircuitResult {
-
-  /**
-   * @param {ResultSet.<DynamicCircuit.DynamicState>} resultSet
-   */
-  constructor( resultSet ) {
-    // @public
-    this.resultSet = resultSet;
-  }
-
-  /**
-   * The time averaged current is used to show transient values in current, such as a current spike when a battery+
-   * capacitor (no resistance) circuit is wired up, see https://phet.unfuddle.com/a#/projects/9404/tickets/by_number/2270?cycle=true
-   * @param {ModifiedNodalAnalysisCircuitElement} element
-   * @returns {number}
-   * @public
-   */
-  getTimeAverageCurrent( element ) {
-    let weightedSum = 0.0;
-    this.resultSet.states.forEach( stateObject => {
-      weightedSum += stateObject.state.dynamicCircuitSolution.getCurrent( element ) * stateObject.dt;
-    } );
-    const number = weightedSum / this.resultSet.getTotalTime();
-    assert && assert( !isNaN( number ) );
-    return number;
-  }
-
-  /**
-   * The instantaneous current is used for computing the next modified nodal analysis state and integration.
-   * @param {ModifiedNodalAnalysisCircuitElement} element
-   * @returns {number}
-   * @public
-   */
-  getInstantaneousCurrent( element ) {
-    return this.getFinalState().dynamicCircuitSolution.getCurrent( element );
-  }
-
-  /**
-   * @param {ModifiedNodalAnalysisCircuitElement} element
-   * @returns {number}
-   * @public
-   */
-  getInstantaneousVoltage( element ) {
-    return this.getFinalState().dynamicCircuitSolution.getVoltage( element );
-  }
-
-  /**
-   * @returns {DynamicState}
-   * @private
-   */
-  getFinalState() {
-    return this.resultSet.getFinalState();
-  }
-}
 
 class DynamicCapacitor {
 
