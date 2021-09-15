ed9e5a4a6d2985282f2c9ce44efb8dab99d0b9c1
samreid <reids@colorado.edu>
2021-09-15 11:57:42 -0600

    Improve CircuitDebugLayer, see https://github.com/phetsims/circuit-construction-kit-common/issues/678

--- js/view/CircuitDebugLayer.js
+++ js/view/CircuitDebugLayer.js
@@ -31,17 +31,51 @@ class CircuitDebugLayer extends Node {
     this.circuitLayerNode.circuit.circuitElements.forEach( circuitElement => {
       const start = circuitElement.startVertexProperty.value.positionProperty.value;
       const end = circuitElement.endVertexProperty.value.positionProperty.value;
-      const arrowNode = new ArrowNode( start.x, start.y, end.x, end.y, {
-        fill: 'red',
-        stroke: 'black',
-        lineWidth: 2
-      } );
+
+      let arrowNode = null;
+      let sign = 0;
+      if ( circuitElement.currentProperty.value > 0 ) {
+        sign = +1;
+      }
+      if ( circuitElement.currentProperty.value < 0 ) {
+        sign = -1;
+      }
+      if ( sign === +1 ) {
+        arrowNode = new ArrowNode( start.x, start.y, end.x, end.y, {
+          fill: 'red',
+          stroke: 'black',
+          lineWidth: 1
+        } );
+      }
+      else if ( sign === -1 ) {
+        arrowNode = new ArrowNode( end.x, end.y, start.x, start.y, {
+          fill: 'red',
+          stroke: 'black',
+          lineWidth: 1
+        } );
+      }
+      else {
+        arrowNode = new ArrowNode( start.x, start.y, end.x, end.y, {
+          fill: 'white',
+          stroke: 'black',
+          lineWidth: 1
+        } );
+      }
+
       this.addChild( arrowNode );
+
+      const offset = end.minus( start ).perpendicular.normalized().timesScalar( 10 );
+      const senseNode = new ArrowNode( start.x + offset.x, start.y + offset.y, end.x + offset.x, end.y + offset.y, {
+        fill: 'green'
+      } );
+      this.addChild( senseNode );
+
       const textNode = new Text( circuitElement.currentProperty.value.toFixed( 2 ) ); //eslint-disable-line
       const panel = new Panel( textNode, {
         center: arrowNode.center,
         fill: circuitElement.currentSenseProperty.value === CurrentSense.FORWARD ? 'red' :
-              circuitElement.currentSenseProperty.value === CurrentSense.BACKWARD ? 'rgba(137, 196, 244, 1)' : 'white'
+              circuitElement.currentSenseProperty.value === CurrentSense.BACKWARD ? 'rgba(137, 196, 244, 1)' :
+              'white'
       } );
       this.addChild( panel );
     } );
