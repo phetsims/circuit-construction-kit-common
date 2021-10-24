98a970ade47907a3630f7c67dda34c0eb30a762e
Sam Reid <reids@colorado.edu>
2021-10-24 10:06:46 -0600

    Remove redundant checks, see https://github.com/phetsims/circuit-construction-kit-common/issues/764

--- js/model/analysis/DynamicCircuitSolution.ts
+++ js/model/analysis/DynamicCircuitSolution.ts
@@ -40,17 +40,9 @@ class DynamicCircuitSolution {
    */
   getCurrent( element: ModifiedNodalAnalysisCircuitElement ) {
 
-    // Scaffolding tests for TypeScript migration
-    if ( element instanceof ModifiedNodalAnalysisCircuitElement ) {
-      assert && assert( element.hasOwnProperty( 'currentSolution' ) );
-    }
-    if ( element instanceof DynamicCapacitor || element instanceof DynamicInductor ) {
-      assert && assert( !element.hasOwnProperty( 'currentSolution' ) );
-    }
-
     // For resistors with r>0, Ohm's Law gives the current.  For components with no resistance (like closed switch or
     // 0-resistance battery), the current is given by the matrix solution.
-    if ( element instanceof ModifiedNodalAnalysisCircuitElement && element.currentSolution !== null ) {
+    if ( element.currentSolution !== null ) {
       return element.currentSolution;
     }
 
