4132ab01feadee8634aa8fac229c09f1c1ea3c67
Sam Reid <reids@colorado.edu>
2021-10-24 10:10:32 -0600

    Update comments, see https://github.com/phetsims/circuit-construction-kit-common/issues/764

--- js/model/analysis/DynamicCircuitResistiveBattery.ts
+++ js/model/analysis/DynamicCircuitResistiveBattery.ts
@@ -5,19 +5,9 @@ class DynamicCircuitResistiveBattery extends ModifiedNodalAnalysisCircuitElement
   readonly voltage: number;
   resistance: number;
 
-  /**
-   * @param {string} node0
-   * @param {string} node1
-   * @param {number} voltage
-   * @param {number} resistance
-   */
   constructor( node0: string, node1: string, voltage: number, resistance: number ) {
     super( node0, node1, null, 0 );
-
-    // @public
     this.voltage = voltage;
-
-    // @public
     this.resistance = resistance;
   }
 }
