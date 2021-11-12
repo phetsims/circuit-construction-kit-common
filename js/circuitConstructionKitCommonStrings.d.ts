3a8eed074d495365511b6a82ce8121816f58c53f
Sam Reid <reids@colorado.edu>
2021-11-11 22:36:42 -0700

    Regenerate string file with grunt modulify, see https://github.com/phetsims/chipper/issues/1139

--- js/circuitConstructionKitCommonStrings.d.ts
+++
@@ -1,5 +0,0 @@
-// Copyright 2021, University of Colorado Boulder
-
-type t = { [ key: string ]: string };
-let circuitConstructionKitCommonStrings: t;
-export default circuitConstructionKitCommonStrings;
---
+++ js/circuitConstructionKitCommonStrings.ts
@@ -0,0 +1,91 @@
+// Copyright 2020-2021, University of Colorado Boulder
+
+/**
+ * Auto-generated from modulify, DO NOT manually modify.
+ */
+/* eslint-disable */
+import getStringModule from '../../chipper/js/getStringModule.js';
+import circuitConstructionKitCommon from './circuitConstructionKitCommon.js';
+
+type StringsType = {
+  'circuit-construction-kit-common': {
+    'title': string
+  },
+  'schematicStandard': string,
+  'realBulb': string,
+  'ieee': string,
+  'iec': string,
+  'british': string,
+  'animationSpeedLimitReached': string,
+  'acSource': string,
+  'advanced': string,
+  'resistor': string,
+  'capacitor': string,
+  'inductor': string,
+  'stopwatch': string,
+  'fuse': string,
+  'time': string,
+  'oneSecond': string,
+  'capacitance': string,
+  'inductance': string,
+  'currentRating': string,
+  'tiny': string,
+  'lots': string,
+  'phaseShift': string,
+  'coin': string,
+  'dollarBill': string,
+  'eraser': string,
+  'pencil': string,
+  'hand': string,
+  'dog': string,
+  'paperClip': string,
+  'wireResistivity': string,
+  'batteryResistance': string,
+  'sourceResistance': string,
+  'battery': string,
+  'lightBulb': string,
+  'addRealBulbs': string,
+  'switch': string,
+  'wire': string,
+  'electrons': string,
+  'conventional': string,
+  'tapCircuitElementToEdit': string,
+  'current': string,
+  'showCurrent': string,
+  'resistance': string,
+  'voltage': string,
+  'voltageWithUnits': string,
+  'voltageChart': string,
+  'currentWithUnits': string,
+  'currentChart': string,
+  'frequency': string,
+  'voltmeter': string,
+  'ammeter': string,
+  'ammeters': string,
+  'labels': string,
+  'values': string,
+  'ohms': string,
+  'voltageUnits': string,
+  'voltageVoltsValuePattern': string,
+  'frequencyHzValuePattern': string,
+  'currentUnits': string,
+  'capacitanceUnits': string,
+  'inductanceUnits': string,
+  'fuseValue': string,
+  'resistanceOhmsSymbol': string,
+  'capacitanceFaradsSymbol': string,
+  'inductanceHenriesSymbol': string,
+  'resistanceOhmsValuePattern': string,
+  'theSwitchIsClosed': string,
+  'theSwitchIsOpen': string,
+  'ammeterReadout': string,
+  'magnitude': string,
+  'signed': string,
+  'dataOutOfRange': string
+};
+
+const circuitConstructionKitCommonStrings = getStringModule( 'CIRCUIT_CONSTRUCTION_KIT_COMMON' ) as StringsType;
+
+circuitConstructionKitCommon.register( 'circuitConstructionKitCommonStrings', circuitConstructionKitCommonStrings );
+
+export default circuitConstructionKitCommonStrings;
