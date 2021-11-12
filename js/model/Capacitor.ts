31c4aeda0c358b2d1abd43b69a8b8a71600f558c
Sam Reid <reids@colorado.edu>
2021-11-12 12:45:53 -0700

    Conserve charge in capacitors, see https://github.com/phetsims/circuit-construction-kit-common/issues/776

--- js/model/Capacitor.ts
+++ js/model/Capacitor.ts
@@ -44,6 +44,39 @@ class Capacitor extends DynamicCircuitElement {
       range: new Range( 0.05, 0.20 ),
       tandem: tandem.createTandem( 'capacitanceProperty' )
     } );
+
+    this.capacitanceProperty.lazyLink( ( c2, c1 ) => {
+
+      const v1 = this.endVertexProperty.value.voltageProperty.value -
+                 this.startVertexProperty.value.voltageProperty.value;
+
+      // we want Q to remain constant under a change of C, so we must adjust V accordingly.
+      // Q1 = C1V1
+      // Q2 = C2V2
+      // Q1 = Q2
+      // C1V1 = C2V2
+      // V2 = C1V1/C2
+
+      const v2 = c1 * v1 / c2;
+
+      // Reflect this in the adjacent local vertices. It doesn't need to flow throughout the circuit because it only affects open circuits
+      const a = this.startVertexProperty.value.voltageProperty.value;
+      const b = this.endVertexProperty.value.voltageProperty.value;
+      const currentDiff = b - a;
+      const desiredDiff = v2;
+      const adjustment = ( desiredDiff - currentDiff ) / 2; // share between the vertices
+
+      this.startVertexProperty.value.voltageProperty.value -= adjustment;
+      this.endVertexProperty.value.voltageProperty.value += adjustment;
+      // this.voltageDiffereneProperty is computed based on those changed values.
+
+      // For debugging
+      // console.log( v2, this.endVertexProperty.value.voltageProperty.value - this.startVertexProperty.value.voltageProperty.value, this.voltageDifferenceProperty.value );
+      // const q1 = c1 * v1;
+      // const q2 = c2 * v2;
+      // console.log( 'q1q2', q1, q2 );
+      this.mnaVoltageDrop = v2;
+    } );
   }
 
   /**
