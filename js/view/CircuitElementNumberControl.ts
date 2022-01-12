3334d622e14d31cda00a7876bf4076f3a26e22b8
Sam Reid <reids@colorado.edu>
2022-01-12 09:15:16 -0700

    Clean up CircuitElementNumberControl, see https://github.com/phetsims/circuit-construction-kit-common/issues/797

--- js/view/CircuitElementNumberControl.ts
+++ js/view/CircuitElementNumberControl.ts
@@ -16,42 +16,26 @@ import CCKCConstants from '../CCKCConstants.js';
 import circuitConstructionKitCommon from '../circuitConstructionKitCommon.js';
 import Circuit from '../model/Circuit.js';
 
-// constants
-const NUMBER_CONTROL_ELEMENT_MAX_WIDTH = 115;
-const READOUT_MAX_WIDTH = 80;
-
 class CircuitElementNumberControl extends NumberControl {
   private readonly disposeCircuitElementNumberControl: () => void;
-  static NUMBER_CONTROL_ELEMENT_MAX_WIDTH: number;
+  static NUMBER_CONTROL_ELEMENT_MAX_WIDTH = 115;
 
-  /**
-   * @param title - text to show as a title
-   * @param valuePattern - pattern for NumberControl to display the value as text
-   * @param valueProperty - property this control changes
-   * @param range
-   * @param circuit - parent circuit
-   * @param numberOfDecimalPlaces - number of decimal places
-   * @param [providedOptions]
-   */
   constructor( title: string, valuePattern: string, valueProperty: Property<number>, range: Range, circuit: Circuit,
                numberOfDecimalPlaces: number, providedOptions?: any ) {
 
-    assert && assert( !!range, 'Range must be provided' );
-
     // When the user changes any parameter of any circuit element, signify it.
+    // TODO: Should this be done through Circuit?  Why is this wired here in the view?  See https://github.com/phetsims/circuit-construction-kit-common/issues/513
     const valuePropertyListener = () => circuit.componentEditedEmitter.emit();
 
     valueProperty.lazyLink( valuePropertyListener );
 
     const options = merge( {
-
-      // subcomponent options
       titleNodeOptions: {
-        maxWidth: NUMBER_CONTROL_ELEMENT_MAX_WIDTH,
+        maxWidth: CircuitElementNumberControl.NUMBER_CONTROL_ELEMENT_MAX_WIDTH,
         font: CCKCConstants.DEFAULT_FONT
       },
       numberDisplayOptions: {
-        maxWidth: READOUT_MAX_WIDTH,
+        maxWidth: 80,
         valuePattern: valuePattern,
         decimalPlaces: numberOfDecimalPlaces,
         textOptions: {
@@ -61,8 +45,6 @@ class CircuitElementNumberControl extends NumberControl {
       layoutFunction: NumberControl.createLayoutFunction1( {
         arrowButtonsXSpacing: 9
       } ),
-
-      // Prevent overlap with the navigation bar
       sliderOptions: {
         thumbTouchAreaYDilation: 5,
         thumbSize: new Dimension2( 10, 20 ),
@@ -75,19 +57,11 @@ class CircuitElementNumberControl extends NumberControl {
     this.disposeCircuitElementNumberControl = () => valueProperty.unlink( valuePropertyListener );
   }
 
-  /**
-   * Dispose resources when no longer used.
-   * @public
-   * @override
-   */
   dispose() {
     super.dispose();
     this.disposeCircuitElementNumberControl();
   }
 }
-
-// @public {number}
-CircuitElementNumberControl.NUMBER_CONTROL_ELEMENT_MAX_WIDTH = NUMBER_CONTROL_ELEMENT_MAX_WIDTH;
 
 circuitConstructionKitCommon.register( 'CircuitElementNumberControl', CircuitElementNumberControl );
 export default CircuitElementNumberControl;
