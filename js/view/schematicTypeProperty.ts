ffbff2652b0a728845abba18de66a85f5a1c7d8d
Sam Reid <reids@colorado.edu>
2022-01-25 16:58:14 -0700

    Instrument schematicTypeProperty, see https://github.com/phetsims/circuit-construction-kit-common/issues/813

--- js/view/schematicTypeProperty.ts
+++ js/view/schematicTypeProperty.ts
@@ -6,11 +6,14 @@
  * @author Sam Reid (PhET Interactive Simulations)
  */
 import StringEnumerationProperty from '../../../axon/js/StringEnumerationProperty.js';
+import Tandem from '../../../tandem/js/Tandem.js';
 import CCKCQueryParameters from '../CCKCQueryParameters.js';
 import circuitConstructionKitCommon from '../circuitConstructionKitCommon.js';
 import SchematicType, { SchematicTypeValues } from './SchematicType.js';
 
-const schematicTypeProperty = new StringEnumerationProperty( SchematicTypeValues, CCKCQueryParameters.schematicStandard as SchematicType );
+const schematicTypeProperty = new StringEnumerationProperty( SchematicTypeValues, CCKCQueryParameters.schematicStandard as SchematicType, {
+  tandem: Tandem.GENERAL_VIEW.createTandem( 'schematicTypeProperty' )
+} );
 
 circuitConstructionKitCommon.register( 'schematicTypeProperty', schematicTypeProperty );
 export default schematicTypeProperty;
