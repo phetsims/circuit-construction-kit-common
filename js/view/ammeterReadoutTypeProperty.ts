b7b828bc88f7c6b1dcd89254544d60663ad6a0bf
Sam Reid <reids@colorado.edu>
2022-01-25 16:59:13 -0700

    Instrument ammeterReadoutTypeProperty, see https://github.com/phetsims/circuit-construction-kit-common/issues/813

--- js/view/ammeterReadoutTypeProperty.ts
+++ js/view/ammeterReadoutTypeProperty.ts
@@ -6,11 +6,14 @@
  * @author Sam Reid (PhET Interactive Simulations)
  */
 import StringEnumerationProperty from '../../../axon/js/StringEnumerationProperty.js';
+import Tandem from '../../../tandem/js/Tandem.js';
 import CCKCQueryParameters from '../CCKCQueryParameters.js';
 import circuitConstructionKitCommon from '../circuitConstructionKitCommon.js';
 import AmmeterReadoutType, { AmmeterReadoutTypeValues } from '../model/AmmeterReadoutType.js';
 
-const ammeterReadoutTypeProperty = new StringEnumerationProperty( AmmeterReadoutTypeValues, CCKCQueryParameters.ammeterReadout as AmmeterReadoutType );
+const ammeterReadoutTypeProperty = new StringEnumerationProperty( AmmeterReadoutTypeValues, CCKCQueryParameters.ammeterReadout as AmmeterReadoutType, {
+  tandem: Tandem.GENERAL_VIEW.createTandem( 'ammeterReadoutTypeProperty' )
+} );
 
 circuitConstructionKitCommon.register( 'ammeterReadoutTypeProperty', ammeterReadoutTypeProperty );
 export default ammeterReadoutTypeProperty;
