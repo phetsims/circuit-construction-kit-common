460175e627f121b03101c4a820fa7dd130bef7d0
Sam Reid <reids@colorado.edu>
2022-01-28 11:17:07 -0700

    Fix argument order, see https://github.com/phetsims/circuit-construction-kit-common/issues/681

--- js/model/PowerProperty.ts
+++ js/model/PowerProperty.ts
@@ -10,7 +10,7 @@ class PowerProperty extends DerivedProperty<number, [ number, number ]> {
   constructor( currentProperty: Property<number>, resistanceProperty: Property<number>, tandem: Tandem ) {
     super(
       [ currentProperty, resistanceProperty ],
-      ( resistance, current ) => Math.abs( current * current * resistance ), {
+      ( current, resistance ) => Math.abs( current * current * resistance ), {
         units: 'W',
         tandem: tandem,
         phetioType: DerivedProperty.DerivedPropertyIO( NumberIO ),
