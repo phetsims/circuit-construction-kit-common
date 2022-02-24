84b5e7ac9f718491d93f88f81e829f0778a09ecc
phet-dev <phetadmin@gmail.com>
2022-02-24 03:21:13 -0700

    update copyright dates from daily grunt work

--- js/view/AmmeterNode.ts
+++ js/view/AmmeterNode.ts
@@ -1,4 +1,4 @@
-// Copyright 2016-2021, University of Colorado Boulder
+// Copyright 2016-2022, University of Colorado Boulder
 
 /**
  * The user interface component with a single probe which reads current values from Wires (not from Vertex or
