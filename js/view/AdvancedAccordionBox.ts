88bdbdd3e00639c11a1f3c2f1fd15d76bac61e30
phet-dev <phetadmin@gmail.com>
2022-03-03 03:21:25 -0700

    update copyright dates from daily grunt work

--- js/view/AdvancedAccordionBox.ts
+++ js/view/AdvancedAccordionBox.ts
@@ -1,4 +1,4 @@
-// Copyright 2020-2021, University of Colorado Boulder
+// Copyright 2020-2022, University of Colorado Boulder
 
 /**
  * Advanced control panel that appears in "Lab" screens which allows the user to adjust the resistivity of wires
