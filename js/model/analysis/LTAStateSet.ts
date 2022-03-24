3056fde7622c663818be501102fd3dd3993a5928
phet-dev <phetadmin@gmail.com>
2022-03-24 03:13:28 -0600

    update copyright dates from daily grunt work

--- js/model/analysis/LTAStateSet.ts
+++ js/model/analysis/LTAStateSet.ts
@@ -1,4 +1,4 @@
-// Copyright 2021, University of Colorado Boulder
+// Copyright 2021-2022, University of Colorado Boulder
 import circuitConstructionKitCommon from '../../circuitConstructionKitCommon.js';
 import MNACircuitElement from './mna/MNACircuitElement.js';
 import LTAState from './LTAState.js';
