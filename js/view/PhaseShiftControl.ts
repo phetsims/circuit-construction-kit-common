6c7652eb58ea8b8f729375601fe50d69c77f9e1c
phet-dev <phetadmin@gmail.com>
2023-01-12 04:14:25 -0700

    update copyright dates from daily grunt work

--- js/view/PhaseShiftControl.ts
+++ js/view/PhaseShiftControl.ts
@@ -1,4 +1,4 @@
-// Copyright 2019-2022, University of Colorado Boulder
+// Copyright 2019-2023, University of Colorado Boulder
 
 /**
  * Control that allows the user to change the phase of the ac voltage source on the AC Lab Screen.
