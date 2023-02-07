7db38dd42640ba5fac71bc3e280f369dcb097288
Matthew Blackman <matthewblackman1@gmail.com>
2023-02-07 17:16:47 -0500

    Simplify AmmeterConnection definition - see https://github.com/phetsims/circuit-construction-kit-common/issues/934

--- js/model/AmmeterConnection.ts
+++ js/model/AmmeterConnection.ts
@@ -6,13 +6,9 @@
  * @author Sam Reid (PhET Interactive Simulations)
  */
 
-import IOType from '../../../tandem/js/types/IOType.js';
-import NumberIO from '../../../tandem/js/types/NumberIO.js';
-import ReferenceIO from '../../../tandem/js/types/ReferenceIO.js';
 import circuitConstructionKitCommon from '../circuitConstructionKitCommon.js';
 import CircuitElement from './CircuitElement.js';
 
-const CircuitElementReferenceIO = ReferenceIO( CircuitElement.CircuitElementIO );
 export default class AmmeterConnection {
   public readonly circuitElement: CircuitElement;
   public readonly current: number;
@@ -21,20 +17,6 @@ export default class AmmeterConnection {
     this.circuitElement = circuitElement;
     this.current = current;
   }
-
-  public static VoltageConnectionIO = new IOType( 'VoltageConnectionIO', {
-    valueType: AmmeterConnection,
-    documentation: 'In order to describe how a Voltmeter probe is connected to a circuit. It indicates an adjacent circuitElement, and the current at the circuitElement. For non-ideal wires, the ' +
-                   'current indicates the partial current dropped up to that point on the wire, like a potentiometer.',
-    toStateObject: currentConnection => ( {
-      circuitElement: CircuitElementReferenceIO.toStateObject( currentConnection.circuitElement ),
-      current: currentConnection.current
-    } ),
-    stateSchema: {
-      circuitElement: CircuitElementReferenceIO,
-      current: NumberIO
-    }
-  } );
 }
 
 circuitConstructionKitCommon.register( 'AmmeterConnection', AmmeterConnection );
