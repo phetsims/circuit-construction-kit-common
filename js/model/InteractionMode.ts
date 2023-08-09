eb935e74fde45e08dfe6aa61810e1825554f678c
phet-dev <phetadmin@gmail.com>
2023-08-09 03:29:53 -0600

    update copyright dates from daily grunt work

--- js/model/InteractionMode.ts
+++ js/model/InteractionMode.ts
@@ -1,4 +1,4 @@
-// Copyright 2021-2022, University of Colorado Boulder
+// Copyright 2021-2023, University of Colorado Boulder
 
 import Enumeration from '../../../phet-core/js/Enumeration.js';
 import EnumerationValue from '../../../phet-core/js/EnumerationValue.js';
