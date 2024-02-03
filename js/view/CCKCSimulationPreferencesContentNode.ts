e1bedc90a81e87555ea50c6212ed5f30ca3eb5db
phet-dev <phetadmin@gmail.com>
2024-02-03 03:16:16 -0700

    update copyright dates from daily grunt work

--- js/view/CCKCSimulationPreferencesContentNode.ts
+++ js/view/CCKCSimulationPreferencesContentNode.ts
@@ -1,4 +1,4 @@
-// Copyright 2020-2023, University of Colorado Boulder
+// Copyright 2020-2024, University of Colorado Boulder
 
 /**
  * Shows contents for controls that change simulation representation or behavior.
