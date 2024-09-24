695d269f86406de3e9dc8908971bd719f95e807e
phet-dev <phetadmin@gmail.com>
2024-09-24 03:25:59 -0600

    update copyright dates from daily grunt work

--- js/CCKCConstants.ts
+++ js/CCKCConstants.ts
@@ -1,4 +1,4 @@
-// Copyright 2015-2023, University of Colorado Boulder
+// Copyright 2015-2024, University of Colorado Boulder
 
 /**
  * Constants used in all of the Circuit Construction Kit sims/screens/scenes.
