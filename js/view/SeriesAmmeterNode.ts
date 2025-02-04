53ba66b266519e7395b4c7484dfd13c8223086a6
phet-dev <phetadmin@gmail.com>
2025-02-04 03:39:07 -0700

    update copyright dates from daily grunt work

--- js/view/SeriesAmmeterNode.ts
+++ js/view/SeriesAmmeterNode.ts
@@ -1,4 +1,4 @@
-// Copyright 2015-2024, University of Colorado Boulder
+// Copyright 2015-2025, University of Colorado Boulder
 
 /**
  * Renders the view for the SeriesAmmeter, which looks the same in lifelike mode or schematic mode.
