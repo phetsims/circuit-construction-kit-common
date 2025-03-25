d4a8e03f28ac0a516fc302c5168669b5b2540c30
Michael Kauzmann <michael.kauzmann@colorado.edu>
2025-03-25 16:32:39 -0600

    runnable *QueryParameters.ts files to use QueryStringMachineModule, https://github.com/phetsims/query-string-machine/issues/54

--- js/CCKCQueryParameters.ts
+++ js/CCKCQueryParameters.ts
@@ -6,6 +6,7 @@
  * @author Sam Reid (PhET Interactive Simulations)
  */
 
+import { QueryStringMachine } from '../../query-string-machine/js/QueryStringMachineModule.js';
 import circuitConstructionKitCommon from './circuitConstructionKitCommon.js';
 
 const CCKCQueryParameters = QueryStringMachine.getAll( {
