7849a471c4b4d9f10f98fd29d7dde30f38ebf94a
Sam Reid <reids@colorado.edu>
2025-04-16 16:05:25 -0600

    Move imports after block comment, see https://github.com/phetsims/perennial/issues/447

--- js/model/AmmeterReadoutType.ts
+++ js/model/AmmeterReadoutType.ts
@@ -1,14 +1,15 @@
 // Copyright 2021-2023, University of Colorado Boulder
 
-import Enumeration from '../../../phet-core/js/Enumeration.js';
-import EnumerationValue from '../../../phet-core/js/EnumerationValue.js';
-import circuitConstructionKitCommon from '../circuitConstructionKitCommon.js';
 
 /**
  * How the ammeter readout is displayed.
  *
  * @author Sam Reid (PhET Interactive Simulations)
  */
+import Enumeration from '../../../phet-core/js/Enumeration.js';
+import EnumerationValue from '../../../phet-core/js/EnumerationValue.js';
+import circuitConstructionKitCommon from '../circuitConstructionKitCommon.js';
+
 export default class AmmeterReadoutType extends EnumerationValue {
   public static readonly MAGNITUDE = new AmmeterReadoutType();
   public static readonly SIGNED = new AmmeterReadoutType();
