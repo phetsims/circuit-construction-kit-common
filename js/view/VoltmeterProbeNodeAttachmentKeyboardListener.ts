bc49f4a1421f6d6dda5d827026aa78ef8bb485dc
phet-dev <phetadmin@gmail.com>
2025-12-02 03:54:10 -0700

    update copyright dates from daily grunt work

--- js/view/VoltmeterProbeNodeAttachmentKeyboardListener.ts
+++ js/view/VoltmeterProbeNodeAttachmentKeyboardListener.ts
@@ -1,4 +1,4 @@
-// Copyright 2016-2025, University of Colorado Boulder
+// Copyright 2025, University of Colorado Boulder
 
 /**
  * Keyboard listener that allows attaching a voltmeter probe to a vertex.
