4e2807f31842d60458355065d1ec9f634adca9c1
Jesse <jesse.greenberg@colorado.edu>
2025-12-19 15:54:28 -0500

    Run modulify after change for https://github.com/phetsims/chipper/issues/1658

--- js/CircuitConstructionKitCommonFluent.ts
+++ js/CircuitConstructionKitCommonFluent.ts
@@ -11,6 +11,7 @@ import FluentPattern from '../../chipper/js/browser/FluentPattern.js';
 import FluentConstant from '../../chipper/js/browser/FluentConstant.js';
 import FluentContainer from '../../chipper/js/browser/FluentContainer.js';
 import FluentComment from '../../chipper/js/browser/FluentComment.js';
+import FluentLibrary from '../../chipper/js/browser-and-node/FluentLibrary.js';
 import circuitConstructionKitCommon from './circuitConstructionKitCommon.js';
 import CircuitConstructionKitCommonStrings from './CircuitConstructionKitCommonStrings.js';
 
@@ -200,7 +201,7 @@ addToMapIfDefined( 'a11y_circuitComponent_accessibleHelpText', 'a11y.circuitComp
 const createFluentFile = (): string => {
   let ftl = '';
   for (const [key, stringProperty] of fluentKeyToStringPropertyMap.entries()) {
-    ftl += `${key} = ${stringProperty.value.replace('\n','\n ')}\n`;
+    ftl += `${key} = ${FluentLibrary.formatMultilineForFtl( stringProperty.value )}\n`;
   }
   return ftl;
 };
