9d6e8a5da2e07eecd8ffa49f04b47ffbc87aca1b
phet-dev <phetadmin@gmail.com>
2026-02-12 03:33:39 -0700

    update copyright dates from daily grunt work

--- js/model/CircuitElementType.ts
+++ js/model/CircuitElementType.ts
@@ -1,4 +1,4 @@
-// Copyright 2025, University of Colorado Boulder
+// Copyright 2025-2026, University of Colorado Boulder
 
 /**
  * CircuitElementType represents the possible types of circuit elements for purposes of i18n and accessibility.
